5fdf06f59cb759daa127bfe4c4e0c952958d43f2
sanipr <sanipujirahayu@gmail.com>
2016-07-21 15:16:15 +0700

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -9,41 +9,64 @@
 
   <title>Arsip LAPAN</title>
 
-  <link rel="apple-touch-icon" href="<?php echo base_url();?>/assets/images/apple-touch-icon.png">
-  <link rel="shortcut icon" href="<?php echo base_url();?>/assets/images/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php echo base_url();?>assets/images/apple-touch-icon.png">
+  <link rel="shortcut icon" href="<?php echo base_url();?>assets/images/favicon.ico">
 
   <!-- Stylesheets -->
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap-extend.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/site.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-extend.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/site.min.css">
 
-  <!--<link rel="stylesheet" href="<?php echo base_url();?>/assets/vendor/animsition/animsition.css">-->
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/vendor/asscrollable/asScrollable.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/vendor/switchery/switchery.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/vendor/intro-js/introjs.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/vendor/slidepanel/slidePanel.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/vendor/flag-icon-css/flag-icon.css">
+  <!--<link rel="stylesheet" href="<?php echo base_url();?>assets/vendor/animsition/animsition.css">-->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/vendor/asscrollable/asScrollable.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/vendor/switchery/switchery.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/vendor/intro-js/introjs.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/vendor/slidepanel/slidePanel.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/vendor/flag-icon-css/flag-icon.css">
 
 
   <!-- Fonts -->
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/fonts/web-icons/web-icons.min.css">
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/fonts/brand-icons/brand-icons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/fonts/web-icons/web-icons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/fonts/brand-icons/brand-icons.min.css">
   <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,300italic'>
 
 
   <!--[if lt IE 9]>
-    <script src="<?php echo base_url();?>/assets/vendor/html5shiv/html5shiv.min.js"></script>
+    <script src="<?php echo base_url();?>assets/vendor/html5shiv/html5shiv.min.js"></script>
     <![endif]-->
 
   <!--[if lt IE 10]>
-    <script src="<?php echo base_url();?>/assets/vendor/media-match/media.match.min.js"></script>
-    <script src="<?php echo base_url();?>/assets/vendor/respond/respond.min.js"></script>
+    <script src="<?php echo base_url();?>assets/vendor/media-match/media.match.min.js"></script>
+    <script src="<?php echo base_url();?>assets/vendor/respond/respond.min.js"></script>
     <![endif]-->
 
   <!-- Scripts -->
-  <script src="<?php echo base_url();?>/assets/vendor/modernizr/modernizr.js"></script>
-  <script src="<?php echo base_url();?>/assets/vendor/breakpoints/breakpoints.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/modernizr/modernizr.js"></script>
+  <script src="<?php echo base_url();?>assets/vendor/breakpoints/breakpoints.js"></script>
   <script>
     Breakpoints();
   </script>
 </head>
+<body style="background-color:#e4eaec;">
+
+  <nav class="site-navbar navbar navbar-default navbar-fixed-top navbar-mega" role="navigation">
+    <div class="navbar-header">
+      <div class="navbar-brand navbar-brand-center site-gridmenu-toggle" data-toggle="">
+        <img class="navbar-brand-logo" src="<?php echo base_url();?>assets/images/logolapan.png" width="50" length="50" alt="...">        
+        <span class="navbar-brand-text">LAPAN</span>
+      </div>
+    </div>
+
+    <div class="navbar-container container-fluid" color="blue">
+      <div class="collapse navbar-collapse navbar-collapse-toolbar" id="site-navbar-collapse">      
+        <div class="nav navbar-toolbar navbar-right navbar-toolbar-right">
+          <li role="presentation">
+            <a href="javascript:void(0)" role="menuitem"><i class="icon wb-power" aria-hidden="true"></i> Keluar</a>
+            <!-- <a href="<?php  echo base_url(); ?>login/login_pegawai" role="menuitem"><i class="icon wb-power" aria-hidden="true"></i> Keluar</a> -->
+            <!-- <a href="javascript:void(0)" role="menuitem"><i class="icon wb-power" aria-hidden="true"></i> Logout</a> -->
+          </li>    
+      </div>
+     </div>
+    </div>
+
+  </nav>
