56435efeaaaf2deccc39bedf005f3e282609a251
sanipr <sanipujirahayu@gmail.com>
2016-07-21 15:45:55 +0700

    Update view_arsip_admin.php

--- application/views/view_arsip_admin.php
+++ application/views/view_arsip_admin.php
@@ -129,7 +129,7 @@
             </li>
             <li class="col-sm-2 col-xs-5">
              <div class="col-md-9">                
-            <button type="button" class="btn btn-raised btn-primary btn-block">Tambah Arsip</button>
+                <button type="button" class="btn btn-block btn-info">Tambah Arsip</button>
 </div>
             </li>
           </ul>
