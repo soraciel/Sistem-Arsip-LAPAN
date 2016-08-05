e6e36c9b016dd1ccaf727e5c7710878a1a522bf7
soraciel <ehem_nuning@yahoo.co.id>
2016-08-05 14:04:29 +0700

    view_user

--- application/views/view_user.php
+++ application/views/view_user.php
@@ -57,7 +57,7 @@
                       <tr>
                         <th>NIP</th>
                         <th>Nama</th>
-                        <th>Status</th>
+                      <!--  <th>Status</th>-->
                         <th class="text-nowrap">Action</th>
                       </tr>
                     </thead>
@@ -71,7 +71,7 @@
                      <tr>
                         <td> <?= $row->ID_PEG ?> </td>
                       <td> <?=  $row->NAMA_PEG ?> </td>
-                    <td><?=  $row->KET ?></td>
+                    <!--<td><?=  $row->KET ?></td>-->
                         <td class="text-nowrap">
                           <button type="button" class="btn btn-sm btn-icon btn-flat btn-default" data-toggle="tooltip"
                           data-original-title="Edit" onclick="location.href='<?php echo base_url();?>index.php/user/edit_user/<?= $row->ID_PEG ?>'">
