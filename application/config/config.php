9db7767d1444d2ea18e5d579c2439576b962a1c9
soraciel <ehem_nuning@yahoo.co.id>
2016-08-10 09:39:37 +0700

    config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Sistem-Arsip-LAPAN/';
+$config['base_url'] = 'http://10.10.9.218:80/Sistem-Arsip-LAPAN/';
 
 /*
 |--------------------------------------------------------------------------
